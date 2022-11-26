7d4b223b820a0df72ccca14829a711f86c351e86
Yoon Gwan <blackgar@naver.com>
2022-11-27 00:37:37 +0900

    재귀 심화 도전 과제 1부 완료

--- Javascript 알고리즘 심화/재귀(Recursion)/도전과제/product_of_array.js
+++ Javascript 알고리즘 심화/재귀(Recursion)/도전과제/product_of_array.js
@@ -1,6 +1,7 @@
 // 문제
 // Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
 
+// 첫 번째 helper 메서드 활용
 const productOfArray = (arr) => {
   let sum = 1;
   const countSum = (numArr) => {
@@ -11,6 +12,14 @@ const productOfArray = (arr) => {
   countSum(arr);
   return sum;
 };
+
+// 두 번쨰 풀이
+// function productOfArray(arr) {
+//   if (arr.length === 0) {
+//     return 1;
+//   }
+//   return arr[0] * productOfArray(arr.slice(1));
+// }
 
 console.log(productOfArray([1, 2, 3])); // 6
 console.log(productOfArray([1, 2, 3, 10])); // 60
---
+++ Javascript 알고리즘 심화/재귀(Recursion)/심화도전과제/reverse.js
@@ -0,0 +1,15 @@
+// 문제
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+
+const reverse = (str) => {
+  if (str.length === 1) return str[0];
+  return reverse([...str].slice(1)) + [...str][0];
+};
+
+// function reverse(str) {
+//   if (str.length <= 1) return str;
+//   return reverse(str.slice(1)) + str[0];
+// }
+
+console.log(reverse("awesome")); // 'emosewa'
+console.log(reverse("rithmschool")); // 'loohcsmhtir'
